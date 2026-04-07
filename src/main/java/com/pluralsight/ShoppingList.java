a43de04c1dbd033cbb7d2875ac37bfbae9d22bd1
lyair <lyairmilliner4work@gmail.com>
2026-04-06 22:02:58 -0400

    Shopping list

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("apples");
+        System.out.println("bread");
+        System.out.println("milk");
+        System.out.println("eggs");
+        System.out.println("rice");
+        System.out.println("chicken");
+        System.out.println("spinach");
+        System.out.println("pasta");
+        System.out.println("tomatoes");
+        System.out.println("coffee");
+    }
+}
